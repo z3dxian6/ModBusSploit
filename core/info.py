519d6852c044ea7a1e4026728ebca0d51b12ad1b
Zoran <88077762+z3dxian6@users.noreply.github.com>
2024-06-05 15:19:01 +0200

    Add files via upload

---
+++ core/info.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from termcolor import colored
+
+def print_info():
+	print("	"+colored("ModBusSploit",'red')+" , Framework for ModBus TCP Industrial Protocol Exploitation")
+	print("")
+	print("	VERSION:	1.1")
+	print("	CODER:		Zзdxian6 aka zзdx")
+	print("	GITHUB:		https://github.com/z3dxian6/ModBusSploit")
+	print("	RELEASE: 	05/06/2024")
+
+def main():
+	print_info()
+
+if __name__ == "__main__" :
+	main()
